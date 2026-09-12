c919adcc63a6bb89f9d5b562a708e58538bf199c
Gouthaminar <gouthaminar@gmail.com>
2026-09-12 23:06:23 +1000

    message

--- test_login.py
+++ test_login.py
@@ -1,12 +1,18 @@
 import random
-
+import os
+from dotenv import load_dotenv
 from playwright.sync_api import expect
 
+load_dotenv()
+
+Base_url = os.environ.get("Base_url")
+Login_username = os.environ.get("Login_username1")
+Login_password = os.environ.get("Login_password1")
 
 def test_login(page):
-    page.goto("https://crm.hanuranext.com")
-    page.get_by_role("textbox", name="Enter username or email").fill("day1verify")
-    page.get_by_test_id("login-password-input").fill("Day1@verify!")
+    page.goto(Base_url)
+    page.get_by_role("textbox", name="Enter username or email").fill(Login_username)
+    page.get_by_test_id("login-password-input").fill(Login_password)
     page.get_by_role("button", name="Sign in").click(timeout=60000)
     expect(page).to_have_url("https://crm.hanuranext.com/login")
     page.get_by_role("link", name="Employees").click()
@@ -24,5 +30,10 @@ def test_login(page):
     # populate hire date using role and name
     page.get_by_role("textbox", name="HIRE DATE").fill("2023-06-01")
     page.get_by_role("button", name="Create employee").click(timeout=15000)
+    page.get_by_role("link", name="Back to employees").click()
+    expect(page).to_have_url("https://crm.hanuranext.com/employees")
+    page.goto("https://crm.hanuranext.com/employees")
+    page.get_by_placeholder("Search by name, title, or email...").fill(f"John Smith{number}")
+    
     page.pause()
 
